2b2b0db37f745fd9d496ed21777906bb6dc88ba2
Yanuar Bima <bima.aji1380@gmail.com>
2023-08-30 22:12:53 +0700

    [FIX] my cv

--- components/Intro/LeftContent.tsx
+++ components/Intro/LeftContent.tsx
@@ -47,7 +47,7 @@ export default function LeftContent() {
 
                 <Link
                     className="group  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer   md:text-base text-sm"
-                    href="/CV.pdf"
+                    href="/YanuarBimaCv.pdf"
                     download
                 >
                     Download CV{" "}
